9637a2d1db79f8d586a7590a07b761f946349382
zacatac <field.zackery@gmail.com>
2014-08-06 23:34:47 -0400

    ugh

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
+
 require 'capistrano/rvm'
-require 'bundler/capistrano'
+
 
 set :application, 'fat_free_crm'
 set :repo_url, 'git@github.com:zacatac/ice.git'
