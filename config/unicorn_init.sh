fb8b580bbab73e1a0c3b1442cbf7dcdd94e7c4a5
zacatac <field.zackery@gmail.com>
2014-08-06 21:36:45 -0400

    explicit bundle path

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -14,7 +14,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/zack/apps/ice/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; ~/.rvm/gems/ruby-2.0.0-p481/bin/bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER="zack"
 set -u
 
